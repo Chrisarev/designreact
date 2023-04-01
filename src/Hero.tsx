8106a0b07375a72c2cfff707e99d6e94cd14432e
Chrisarev <CHRIS.AREV@GMAIL.COM>
2023-03-31 20:40:03 -0700

    Added first load gradient animation and first panel of homepage

---
+++ src/Hero.tsx
@@ -0,0 +1,19 @@
+import styles from './stylesheets/Hero.module.css' 
+
+const Hero = () =>{
+
+
+    return (
+        <>
+        <div className={styles.firstLoadPanel}>
+            <div className={styles.introText}>Experience</div>
+            <div className={styles.introText}>Everything</div>
+        </div>
+        <div className={styles.panel}>
+
+        </div>
+        </>
+    )
+}
+
+export default Hero;
