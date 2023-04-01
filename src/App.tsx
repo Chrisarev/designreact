c07ad497a4eb968c3f4e9c14e377795f88696d28
Chrisarev <CHRIS.AREV@GMAIL.COM>
2023-03-31 21:13:24 -0700

    Added navbar and fadeIn animation on load

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Hero from './Hero'
-
+import Navbar from './Navbar'
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Hero />
     </div>
   );
