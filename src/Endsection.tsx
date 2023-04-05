cc528d3daaae484e89948d3f0a298de6379f76d7
Chrisarev <CHRIS.AREV@GMAIL.COM>
2023-04-04 21:18:17 -0700

    Added and styled services section, added scroll animation to cards

--- src/Endsection.tsx
+++ src/Endsection.tsx
@@ -1,12 +1,40 @@
 import styles from './stylesheets/Endsection.module.css' 
+import {useScroll, useTransform, motion} from 'framer-motion'
+import {useRef} from 'react'
 
 const Endsection = () =>{
+    //Need the useRef hook to get position of parent element 
+    const targetRef = useRef<HTMLDivElement>(null); 
+    //Ref is the target of useScroll hook
+    //number scrollYProgress contains num between 0-1 referring to 
+    //how far user has scrolled in element 
+    const {scrollYProgress} = useScroll({
+        target:targetRef,
+        //takes two strings ex:["start end", "end end"] which describe intersections between the target
+        //and a container element(container is window if not defined)
+        //Any range between those intersections is where scroll progress is applied
+        offset: ["start end", "end start"]
+    })
 
+    const translateY = useTransform(scrollYProgress, [0,0.5,0.6,0.75,1], [200,0,0,-150,0] )
+    const translateY2 = useTransform(scrollYProgress, [0,0.5,0.6,0.75,1], [300,0,0,-200,0] )
+    const translateY3 = useTransform(scrollYProgress, [0,0.5,0.6,0.75,1], [400,0,0,-300,0] )
 
     return (
         <>
-            <div className={styles.panel}>
-                
+            <div ref={targetRef} className={styles.panel}>
+                <h1>Services we offer</h1>
+                <div className={styles.cardHolder}>
+                    <motion.div style={{translateY}} className={styles.serviceCard}>
+                        Card 1
+                    </motion.div>
+                    <motion.div style={{translateY:translateY2}} className={styles.serviceCard}>
+                        Card 2
+                    </motion.div>
+                    <motion.div style={{translateY:translateY3}}className={styles.serviceCard}>
+                        Card 3
+                    </motion.div>
+                </div>
             </div>
         </>
     )
--- src/Hero.tsx
+++ src/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () =>{
             <div className={styles.introText}>Everything</div>
         </div>
         <div className={styles.panel}>
-            <div className={styles.header}>Experiences Design Firm</div>
+            <div className={styles.header}>Experiences Design</div>
             <div className={styles.subHeader}>We bridge your vision with the experience of your users.</div>
         </div>
         </>
