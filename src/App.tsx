b296072b0c7f498371b2fe8a2d26a4b3c2d43f08
Chrisarev <CHRIS.AREV@GMAIL.COM>
2023-04-06 15:34:07 -0700

    Renamed components and adjusted servicepanel animations

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import './App.css';
 import Hero from './Hero'
 import Navbar from './Navbar'
 import Cardpanel from './Cardpanel'
+import Impactpanel from './Impactpanel'
 import Servicespanel from './Servicespanel'
-import Endsection from './Endsection'
 import Footer from './Footer'
 
 function App() {
@@ -14,8 +14,8 @@ function App() {
       <Navbar />
       <Hero />
       <Cardpanel />
+      <Impactpanel/>
       <Servicespanel />
-      <Endsection />
       <Footer />
     </div>
   );
